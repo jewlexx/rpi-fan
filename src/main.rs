f00d3b2905be43cd820ea34b98e8fe354272d446
Juliette Cordor <grassojames5@gmail.com>
2022-05-15 12:32:47 +1000

    fixes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rppal::{gpio::Gpio, system::DeviceInfo};
 
 const FAN_PIN: u8 = 14;
 
-fn main() -> anyhow::Result<!> {
+fn main() -> anyhow::Result<()> {
     let info = DeviceInfo::new()?;
     let model = info.model();
 
@@ -14,5 +14,5 @@ fn main() -> anyhow::Result<!> {
 
     pin.toggle();
 
-    loop {}
+    Ok(())
 }
