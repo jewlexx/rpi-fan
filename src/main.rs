68f0aeb805bea0d620b338703c04171916bbe93a
Juliette Cordor <grassojames5@gmail.com>
2022-05-15 12:32:18 +1000

    added files

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![feature(never_type)]
+
+use rppal::{gpio::Gpio, system::DeviceInfo};
+
+const FAN_PIN: u8 = 14;
+
+fn main() -> anyhow::Result<!> {
+    let info = DeviceInfo::new()?;
+    let model = info.model();
+
+    println!("{}", model);
+
+    let mut pin = Gpio::new()?.get(FAN_PIN)?.into_output();
+
+    pin.toggle();
+
+    loop {}
+}
