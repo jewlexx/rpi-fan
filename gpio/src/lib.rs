2296df466893cba910d038f8b1b1b146bde9996e
Juliette Cordor <grassojames5@gmail.com>
2022-05-17 10:54:25 +1000

    fixed opt deps

--- gpio/src/lib.rs
+++ gpio/src/lib.rs
@@ -1,5 +1,5 @@
 cfg_if::cfg_if! {
-    if #[cfg(target = "aarch64-unknown-linux-gnu")] || #[cfg(target = "aarch64-unknown-linux-musl")] {
+    if #[cfg(target = "aarch64-unknown-linux-gnu")] {
         pub use rppal::gpio::*;
     } else {
         mod fake;
