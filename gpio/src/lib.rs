4c7cc7447f78ec5c84bfd1f5e2347aff88972212
Juliette Cordor <grassojames5@gmail.com>
2022-05-17 10:53:06 +1000

    added fake library

--- gpio/src/lib.rs
+++ gpio/src/lib.rs
@@ -1,1 +1,9 @@
-pub use rppal::gpio::*;
+cfg_if::cfg_if! {
+    if #[cfg(target = "aarch64-unknown-linux-gnu")] || #[cfg(target = "aarch64-unknown-linux-musl")] {
+        pub use rppal::gpio::*;
+    } else {
+        mod fake;
+        pub use fake::*;
+    }
+}
+
