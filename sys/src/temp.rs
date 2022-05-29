ad8c478e202171b48d0bab1427407543777f1ae6
Juliette Cordor <grassojames5@gmail.com>
2022-05-29 20:30:31 +1000

    fixes

--- sys/src/temp.rs
+++ sys/src/temp.rs
@@ -1,7 +1,9 @@
+use std::fs::{File, Read};
+
 pub const TEMPERATURE_PATH: &str = "/sys/class/thermal/thermal_zone0/temp";
 
 pub fn get_temp() -> i128 {
-    let mut file = std::fs::File::open(TEMPERATURE_PATH).unwrap();
+    let mut file = File::open(TEMPERATURE_PATH).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
