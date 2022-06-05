4edbf16fed3ac805c5989f42b391c34bc6891c8e
Juliette Cordor <grassojames5@gmail.com>
2022-06-05 13:37:05 +1000

    fixes

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,10 +1,18 @@
 /// Converts a try_lock [`Option`] into a [`Result`]
 #[macro_export]
 macro_rules! lock_inner {
-    ($lock:expr) => {
+    [ $lock:expr ] => {
         match $lock.try_lock() {
             Some(guard) => Ok(guard),
             None => Err($crate::FanError::LockError),
         }
+    };
+}
+
+/// Locks the given mutex and blocks the current thread until it can be locked
+#[macro_export]
+macro_rules! lock_inner_blocking {
+    [ $lock:expr ] => {
+        $lock.lock()
     };
 }
