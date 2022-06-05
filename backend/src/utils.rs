7be008260be2904017fbea7b8d85602254a1c42b
Juliette Cordor <grassojames5@gmail.com>
2022-06-05 13:39:10 +1000

    fixes

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,11 +1,19 @@
-/// Converts a try_lock [`Option`] into a [`Result`]
+/// Converts an [`Option`] to a [`Result`] with the provided error.
+#[macro_export]
+macro_rules! opt_to_res {
+    [ $opt:expr, $res:expr ] => {
+        match $opt {
+            Some(guard) => Ok(guard),
+            None => Err($res),
+        }
+    };
+}
+
+/// Converts a tries to lock the given mutex and returns a [`FanError`] if the lock fails.
 #[macro_export]
 macro_rules! lock_inner {
     [ $lock:expr ] => {
-        match $lock.try_lock() {
-            Some(guard) => Ok(guard),
-            None => Err($crate::FanError::LockError),
-        }
+        $crate::opt_to_res!($lock.try_lock(), $crate::FanError::LockError)
     };
 }
 
